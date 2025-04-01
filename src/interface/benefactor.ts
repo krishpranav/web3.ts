272bfc81a27f09e4c54373529f89e43aca041b20
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-04-01 19:35:58 +0530

    src: interface[benefactor]

---
+++ src/interface/benefactor.ts
@@ -0,0 +1,11 @@
+/**
+ * @author: Krisna Pranav
+ * @file: benefactor.ts
+ * @copyright Copyright (c) 2025 Krisna Pranav
+ */
+
+export interface Benefactor {
+    benefactor: string;
+    amount: string | number;
+    creationDate: number;
+}
