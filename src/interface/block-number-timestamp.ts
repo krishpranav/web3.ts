baab8fe5e449b40a5f1fb9527f50444af20b2106
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-04-01 20:18:01 +0530

    interface: block-number-timestamp

---
+++ src/interface/block-number-timestamp.ts
@@ -0,0 +1,10 @@
+/**
+ * @author: Krisna Pranav
+ * @file: benefactor.ts
+ * @copyright Copyright (c) 2025 Krisna Pranav
+ */
+
+export interface BlockNumberAndTimestamp {
+    blockNumber: number;
+    timestamp: number;
+}
