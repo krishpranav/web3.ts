9e631abe8ed3cc76465ffc32a6b886d586d9f275
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-04-01 20:18:09 +0530

    interface: proposal-detail

---
+++ src/interface/proposal-detail.ts
@@ -0,0 +1,10 @@
+/**
+ * @author: Krisna Pranav
+ * @file: proposal-detail.ts
+ * @copyright Copyright (c) 2025 Krisna Pranav
+ */
+
+export interface ProposalDetail {
+    recipient: string;
+    percentage: number;
+}
