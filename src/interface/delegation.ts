950c80c5c6d8c88e97733f0692f0a4a396284050
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-04-01 21:44:40 +0530

    interface: delegation

---
+++ src/interface/delegation.ts
@@ -0,0 +1,12 @@
+/**
+ * @author: Krisna Pranav
+ * @file: delegation.ts
+ * @copyright Copyright (c) 2025 Krisna Pranav
+ */
+
+export interface Delegation {
+    id: number;
+    from: string;
+    to: string;
+    amount: string | number;
+}
